79c9511f311ebb66c7027e93ac4d43b1bcba11d1
baltikaa9 <sergey4016@mail.ru>
2023-11-14 16:12:33 +0300

    lr4

--- LR4/logic/presets.php
+++ LR4/logic/presets.php
@@ -1,6 +1,5 @@
 <?php
 function get_preset(int $number): string {
-//    вырезать body
     $html = match ($number) {
         1 => file_get_contents('https://ru.wikipedia.org/wiki/%D0%9A%D0%B8%D0%BD%D0%BE%D1%80%D0%B8%D0%BD%D1%85%D0%B8'),
         2 => file_get_contents('https://www.gazeta.ru/culture/2021/12/16/a_14322589.shtml'),
